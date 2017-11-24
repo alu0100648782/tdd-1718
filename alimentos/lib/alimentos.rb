54e4caf3b6d7a4bb7ffdb27ea8f84b15f88b5edf
rafael abadia <alu0100648782@ull.edu.es>
2017-11-24 11:38:16 +0000

    Codigo Practica 9

--- alimentos/lib/alimentos.rb
+++ alimentos/lib/alimentos.rb
@@ -2,6 +2,9 @@ require "alimentos/version"
 require "alimentos/alimento"
 require "alimentos/lista"
 require "alimentos/grupo"
+require "alimentos/comida"
+require "alimentos/individuo"
+require "alimentos/calculadora"
 
 module Alimentos
   # Your code goes here...
--- alimentos/spec/alimentos_spec.rb
+++ alimentos/spec/alimentos_spec.rb
@@ -456,5 +456,42 @@ describe Alimentos do
 		expect(@grupo <= @grupo_pescados).to eq(true)
 	end
   end  
+  
+
+  context 'Comidas' do
+	before :each do	
+	        @compota1 = Comida.new('Compota', '5',     [6.7,6.5,6.8,6.9,7.0,7.1,6.9,6.9,6.9,6.7,6.9,7.3,7.0,7.0,7.2,7.1,6.8,7.2,7.3,7.0,6.8,6.7,6.8,6.7,6.9])
+	        @yogurt1 = Comida.new('Yogurt', '5',       [6.1,6.6,6.3,6.3,6.1,6.9,6.8,6.5,6.4,6.9,6.8,6.5,6.3,6.2,6.7,6.2,5.9,5.8,5.8,5.8,5.8,5.8,5.9,6.2,6.4])
+	        @chocolate1 = Comida.new('Chocolate', '5', [6.5,6.5,6.7,6.5,6.5,6.8,6.7,6.2,6.5,7.2,6.9,7.0,6.3,6.2,6.1,5.9,5.8,6.1,6.7,6.7,6.6,6.7,6.9,7.2,7.1])
+		@glucosa1 = Comida.new('Glucosa', '5',     [4.9,5.3,5.9,6.7,7.2,7.6,8.0,8.2,8.2,8.4,8.3,8.3,8.0,7.5,7.1,6.8,6.8,6.9,6.8,6.3,6.2,6.3,6.2,6.3,6.1])
+		@individuo1 = Individuo.new('1',[@compota1,@yogurt1,@chocolate1],@glucosa1)
+
+	        @compota2 = Comida.new('Compota', '5',    [4.6,4.8,5.3,5.6,6.1,6.5,6.6,7.0,7.0,6.8,6.4,6.3,6.1,6.1,6.2,6.0,6.1,6.1,6.2,6.3,6.4,6.1,6.1,5.7,5.9])
+	        @yogurt2 = Comida.new('Yogurt', '5',      [4.9,4.9,5.2,5.8,6.5,7.0,7.2,7.3,7.3,6.6,6.2,6.1,6.0,6.1,5.9,5.9,5.9,5.9,5.8,5.8,5.5,5.5,5.6,5.9,5.9])
+	        @chocolate2 = Comida.new('Chocolate', '5',[4.6,4.6,4.7,4.7,4.8,4.7,4.8,4.8,4.6,4.4,4.7,4.7,4.8,4.7,5.2,5.9,5.9,5.7,5.4,5.3,5.1,4.8,4.8,4.9,5.9])
+		@glucosa2 = Comida.new('Glucosa', '5',    [6.3,5.4,5.6,5.7,6.5,7.4,7.9,7.4,7.7,7.9,7.9,7.8,7.8,7.8,8.0,8.5,9.4,10.8,10.5,9.1,8.9,8.3,7.7,7.6,7.5])
+		@individuo2 = Individuo.new('2',[@compota2,@yogurt2,@chocolate2],@glucosa2)
+
+		@calculadora1 = Calculadora.new([@individuo1, @individuo2])
+	end
+
+
+	it 'Compota existe' do
+        	expect(@compota1.to_s).to be == "\nCompota\n\nConcentraciones: [6.7, 6.5, 6.8, 6.9, 7.0, 7.1, 6.9, 6.9, 6.9, 6.7, 6.9, 7.3, 7.0, 7.0, 7.2, 7.1, 6.8, 7.2, 7.3, 7.0, 6.8, 6.7, 6.8, 6.7, 6.9]\nPeriodo de Tiempo: 5 minutos\n\nAIBC: 28.0"
+    	end
+
+	it 'Individuo existe' do
+	        expect(@individuo1.to_s).to be == "\Individuo 1\nAlimentos:\n\nCompota\n\nConcentraciones: [6.7, 6.5, 6.8, 6.9, 7.0, 7.1, 6.9, 6.9, 6.9, 6.7, 6.9, 7.3, 7.0, 7.0, 7.2, 7.1, 6.8, 7.2, 7.3, 7.0, 6.8, 6.7, 6.8, 6.7, 6.9]\nPeriodo de Tiempo: 5 minutos\n\nAIBC: 28.0\nYogurt\n\nConcentraciones: [6.1, 6.6, 6.3, 6.3, 6.1, 6.9, 6.8, 6.5, 6.4, 6.9, 6.8, 6.5, 6.3, 6.2, 6.7, 6.2, 5.9, 5.8, 5.8, 5.8, 5.8, 5.8, 5.9, 6.2, 6.4]\nPeriodo de Tiempo: 5 minutos\n\nAIBC: 30.5\nChocolate\n\nConcentraciones: [6.5, 6.5, 6.7, 6.5, 6.5, 6.8, 6.7, 6.2, 6.5, 7.2, 6.9, 7.0, 6.3, 6.2, 6.1, 5.9, 5.8, 6.1, 6.7, 6.7, 6.6, 6.7, 6.9, 7.2, 7.1]\nPeriodo de Tiempo: 5 minutos\n\nAIBC: 18.5\nGlucosa\n\nConcentraciones: [4.9, 5.3, 5.9, 6.7, 7.2, 7.6, 8.0, 8.2, 8.2, 8.4, 8.3, 8.3, 8.0, 7.5, 7.1, 6.8, 6.8, 6.9, 6.8, 6.3, 6.2, 6.3, 6.2, 6.3, 6.1]\nPeriodo de Tiempo: 5 minutos\n\nAIBC: 256.0\n"
+	end
+
+	it 'Calculadora existe' do
+		expect(@calculadora1.to_s).to be == "\nLista de Individuos y sus alimentos\nIndividuo 1\nAlimentos:\n\nCompota\n\nConcentraciones: [6.7, 6.5, 6.8, 6.9, 7.0, 7.1, 6.9, 6.9, 6.9, 6.7, 6.9, 7.3, 7.0, 7.0, 7.2, 7.1, 6.8, 7.2, 7.3, 7.0, 6.8, 6.7, 6.8, 6.7, 6.9]\nPeriodo de Tiempo: 5 minutos\n\nAIBC: 28.0\nYogurt\n\nConcentraciones: [6.1, 6.6, 6.3, 6.3, 6.1, 6.9, 6.8, 6.5, 6.4, 6.9, 6.8, 6.5, 6.3, 6.2, 6.7, 6.2, 5.9, 5.8, 5.8, 5.8, 5.8, 5.8, 5.9, 6.2, 6.4]\nPeriodo de Tiempo: 5 minutos\n\nAIBC: 30.5\nChocolate\n\nConcentraciones: [6.5, 6.5, 6.7, 6.5, 6.5, 6.8, 6.7, 6.2, 6.5, 7.2, 6.9, 7.0, 6.3, 6.2, 6.1, 5.9, 5.8, 6.1, 6.7, 6.7, 6.6, 6.7, 6.9, 7.2, 7.1]\nPeriodo de Tiempo: 5 minutos\n\nAIBC: 18.5\nGlucosa\n\nConcentraciones: [4.9, 5.3, 5.9, 6.7, 7.2, 7.6, 8.0, 8.2, 8.2, 8.4, 8.3, 8.3, 8.0, 7.5, 7.1, 6.8, 6.8, 6.9, 6.8, 6.3, 6.2, 6.3, 6.2, 6.3, 6.1]\nPeriodo de Tiempo: 5 minutos\n\nAIBC: 256.0\n\nIndividuo 2\nAlimentos:\n\nCompota\n\nConcentraciones: [4.6, 4.8, 5.3, 5.6, 6.1, 6.5, 6.6, 7.0, 7.0, 6.8, 6.4, 6.3, 6.1, 6.1, 6.2, 6.0, 6.1, 6.1, 6.2, 6.3, 6.4, 6.1, 6.1, 5.7, 5.9]\nPeriodo de Tiempo: 5 minutos\n\nAIBC: 183.25\nYogurt\n\nConcentraciones: [4.9, 4.9, 5.2, 5.8, 6.5, 7.0, 7.2, 7.3, 7.3, 6.6, 6.2, 6.1, 6.0, 6.1, 5.9, 5.9, 5.9, 5.9, 5.8, 5.8, 5.5, 5.5, 5.6, 5.9, 5.9]\nPeriodo de Tiempo: 5 minutos\n\nAIBC: 138.5\nChocolate\n\nConcentraciones: [4.6, 4.6, 4.7, 4.7, 4.8, 4.7, 4.8, 4.8, 4.6, 4.4, 4.7, 4.7, 4.8, 4.7, 5.2, 5.9, 5.9, 5.7, 5.4, 5.3, 5.1, 4.8, 4.8, 4.9, 5.9]\nPeriodo de Tiempo: 5 minutos\n\nAIBC: 44.75\nGlucosa\n\nConcentraciones: [6.3, 5.4, 5.6, 5.7, 6.5, 7.4, 7.9, 7.4, 7.7, 7.9, 7.9, 7.8, 7.8, 7.8, 8.0, 8.5, 9.4, 10.8, 10.5, 9.1, 8.9, 8.3, 7.7, 7.6, 7.5]\nPeriodo de Tiempo: 5 minutos\n\nAIBC: 196.0\n\n"
+	end
+
+	it 'Calculos existen' do
+		expect(@calculadora1.mostrar_resultados).to be == "Resultados\nCompota 52.22\nYogurt 41.29\nChocolate 15.03\n"
+	end
+  end
+  
 
 end
